1a910c0159d18500f74a91cb5b6564f22ff6f083
Mago805 <137172463+Mago805@users.noreply.github.com>
2024-11-27 17:10:30 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
     'Diário de Bordo', 
     'Qual foi o destino?', 
-    'Informar o destino da viagem.', 
+    'Berlim.', 
 )
 
 criaCartao(
     'Custo da Viagem', 
     'Qual foi o custo total dessa viagem por pessoa e qual a modalidade de aquisição?', 
-    'Informar o valor na moeda original do local e o valor convertido para R$. Informar se a aquisição foi via agência de viagens ou aquisição de forma independente '
+    'Fomos via pacote da CVC por 30.664,49 convertido para Euro ficou 4906,32. Nós ficamos em um hotel bem centralizado em Berlim para poder passear em todos o pontos turisticos gratuitos.'
 )
 
 criaCartao(
     'Fato Curioso', 
     'Uma curiosidade sobre cidade/ponto turístico/país cisitado!', 
-    'Informar através de um breve texto uma curiosidade.'
+    'A Alemanha é famosa por sua tradição cervejeira, com mais de 1.300 cervejarias, e pela Oktoberfest, a maior festa de cerveja do mundo, realizada em Munique. Além disso, o conceito de "Gemütlichkeit" reflete um sentimento acolhedor e confortável, associado à convivência agradável, boa comida e bebida, sendo um aspecto importante da cultura social alemã.'
 )
 
 criaCartao(
     'Passeios', 
     'Onde passear?', 
-    'Informar os locais, breve resumo do que são.'
+    'Catedral de Berlim é o edifício religioso mais representativo da cidade. Sua cúpula oferece as melhores vistas de Berlim. A Catedral de Berlim (Berliner Dom) se levanta majestosa nas proximidades do Rio Spree, coroada por uma cúpula de cobre de cor esverdeada.'
 )
 
 criaCartao(
     'Prato Típico', 
-    'Conhece o "nome do prato"?', 
-    'Explicar sobre o prato, porque o escolheu e o ingrediente principal.'
+    'Conhece o "Pretzel"?', 
+    'Pretzel Descrição: Um pão salgado em formato trançado, com uma crosta dourada e textura macia por dentro. Significado Cultural: Um ícone da culinária alemã, frequentemente servido com cerveja. Receita Simples: Ingredientes: Farinha, água, fermento, manteiga, bicarbonato de sódio e sal grosso. Substituições: Sal grosso pode ser trocado por sementes de gergelim.'
 )
 
 criaCartao(
     'Ponto de Vista', 
     'O que eu achei da viagem para ...?', 
-    'Informar através de um breve texto sobre o que achou da viagem, o que faria de diferente e por quê.'
+    'Em nossa visita a Berlim foi incrível! A cidade combina história e modernidade de forma única. Fiquei encantado com a grandiosidade do Portão de Brandemburgo e a beleza arquitetônica do Berliner Dom. A experiência foi enriquecida pela gastronomia local: os doces krapfen foram perfeitos para um lanche da tarde, enquanto os pretzels eram ideais para um dia de caminhada pela cidade..'
 )
